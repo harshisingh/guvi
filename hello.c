f6f2e157bcbc6bf15bcb2842cf3cc4c53bc8119b
harshisingh <35405305+harshisingh@users.noreply.github.com>
2018-01-15 23:04:53 +0530

    Create hello.c

---
+++ hello.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int N,i;
+	printf("Enter the value of N\n");
+	scanf("%d",&N);
+	for(i=0;i<N;i++)
+	printf("Hello\n");
+	return 0;
+}
